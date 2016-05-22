06077e70a10a8f3fd348a08ee097afdf5cfc2528
Michael Lenzen <m.lenzen@gmail.com>
2016-05-22 01:36:48 -0500

    Copy in code.

--- flask_lenzm_utils/__init__.py
+++ flask_lenzm_utils/__init__.py
@@ -6,18 +6,8 @@ Flask_LenzM_Utils
 Utils for Flask Projects
 
 :copyright: (c) 2016 by Michael Lenzen
-:licence: BSD, see LICENCE for more details
+:licence: MIT, see LICENCE for more details
 """
-from __future__ import absolute_import, unicode_literals
 
-__title__ = 'Flask-LenzM-Utils'
-__summary__ = 'Utils for Flask Projects'
-__uri__ = 'https://github.com/mlenzen/Flask-LenzM-Utils'
+from . import flask_db_admin, url_for_obj, url_update
 
-__version__ = '0.0.1'
-
-__author__ = 'Michael Lenzen'
-__email__ = 'm.lenzen@gmail.com'
-
-__license__ = 'BSD'
-__copyright__ = 'Copyright 2016 Michael Lenzen'
---
+++ flask_lenzm_utils/flask_db_admin/pg.py
@@ -0,0 +1,50 @@
+"""click interface for postgresql commands.
+
+"""
+import logging
+import os
+import subprocess
+
+import click
+from flask import current_app
+
+DEFAULT_DB_BACKUP_PATH = 'db.pg_dump'
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+cli = click.Group(help=__doc__, name='pg')
+
+
+@cli.command()
+@click.option('-l', '--location', default=DEFAULT_DB_BACKUP_PATH)
+def backup(location):
+	'''Run pg_dump.'''
+	os.environ['PGPASSWORD'] = current_app.config['PG_PASSWORD']
+	# /usr/pgsql-9.3/bin/pg_dump -Fd analytics -f analytics-dump --username=analytics
+	pg_dump = current_app.config['PG_BIN_DIR'] + 'pg_dump'
+	subprocess.call((
+		pg_dump,
+		'--host={}'.format(current_app.config['PG_HOST']),
+		'--username={}'.format(current_app.config['PG_USERNAME']),
+		'--format=c',
+		current_app.config['PG_DB_NAME'],
+		'--file=%s' % location,
+		))
+
+
+@cli.command()
+@click.option('-l', '--location', default=DEFAULT_DB_BACKUP_PATH)
+def restore(location):
+	'''Restore pg_dump.'''
+	os.environ['PGPASSWORD'] = current_app.config['PG_PASSWORD']
+	# pg_restore -h localhost -U analytics -d analytics --clean db-backup/
+	pg_restore = current_app.config['PG_BIN_DIR'] + 'pg_restore'
+	subprocess.call((
+		pg_restore,
+		'--host={}'.format(current_app.config['PG_HOST']),
+		'--username={}'.format(current_app.config['PG_USERNAME']),
+		'--dbname={}'.format(current_app.config['PG_DB_NAME']),
+		'--clean',
+		location,
+		))
