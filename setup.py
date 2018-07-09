5c3b92cb1d347505edaba84f36ee626f216d5247
Michael Lenzen <m.lenzen@gmail.com>
2018-07-09 02:20:21 -0500

    Bump version: 0.8.1 → 0.8.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
 	name='lenzm_utils',
-	version='0.8.1',
+	version='0.8.2',
 	description='Various utils including Flask projects',
 	long_description=long_description,
 	author='Michael Lenzen',
